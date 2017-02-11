59480d73564b1ad212545cbfc5e7837ba33d81aa
kbanico <code4uguam@gmail.com>
2017-02-11 15:44:15 +1000

    make friendship actions possible

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: "registrations"}
   root 'pages#home'
   resources :users, only: [:show, :index]
+  resources :friendships, only: [:create, :destroy, :accept] do
+    member do
+      put :accept
+    end
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
